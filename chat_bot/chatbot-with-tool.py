01db5dea9ca6e17105af09d1e3ef9d2b30d3d850
vikashkumardatascience007 <vikash.datascience.007@gmail.com>
2025-12-21 15:30:50 +0530

    with tool code

---
+++ chat_bot/chatbot-with-tool.py
@@ -0,0 +1,57 @@
+from typing import TypedDict, Annotated
+from langgraph.graph import add_messages, StateGraph, END
+from langchain_ollama import ChatOllama
+from langchain_core.messages import AIMessage, HumanMessage
+from dotenv import load_dotenv
+from langchain_tavily import TavilySearchResults
+from langgraph.prebuilt import ToolNode
+
+load_dotenv()
+
+class BasicChatBot(TypedDict):
+    messages: Annotated[list, add_messages]
+    
+search_tool = TavilySearchResults(max_results=2)
+tools = [search_tool]
+
+llm = ChatOllama(model="llama3.2:latest")
+
+llm_with_tool=llm.bind_tools(tools=tools)
+
+def chatbot(state: BasicChatBot):
+    return {
+        "messages": [llm_with_tools.invoke(state["messages"])], 
+    }
+
+
+def tools_router(state:BasicChatBot):
+    last_message=state["messages"][-1]
+    if(hasattr(last_messages,'tool_calls') and len(last_message.tool_calls)>0):
+        return "tool_node"
+    else: 
+        return END
+    
+tool_node = ToolNode(tools=tools)
+
+graph = StateGraph(BasicChatBot)
+
+graph.add_node("chatbot", chatbot)
+graph.add_node("tool_node", tool_node)
+graph.set_entry_point("chatbot")
+
+graph.add_conditional_edges("chatbot", tools_router)
+graph.add_edge("tool_node", "chatbot")
+
+app = graph.compile()
+
+while True: 
+    user_input = input("User: ")
+    if(user_input in ["exit", "end"]):
+        break
+    else: 
+        result = app.invoke({
+            "messages": [HumanMessage(content=user_input)]
+        })
+
+        print(result)
+    
